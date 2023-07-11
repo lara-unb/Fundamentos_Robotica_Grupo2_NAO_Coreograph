6ef15e3c7ae9b38c39365dc05f82c57bbf16b5d7
Herbert T <131397653+mrsherbert@users.noreply.github.com>
2023-07-11 11:24:18 -0300

    Mudancas

---
+++ Projeto 4/Codigo_Projeto_4/translations/translation_en_US.ts
@@ -0,0 +1,54 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Algo a frente a direita</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Algo a frente a direita</translation>
+        </message>
+        <message>
+            <source>Algo a direita</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Algo a direita</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>algo a frente </source>
+            <comment>Text</comment>
+            <translation type="unfinished">algo a frente </translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (2)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Algo a frente na esquerda</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Algo a frente na esquerda</translation>
+        </message>
+        <message>
+            <source>Algo a esquerda</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Algo a esquerda</translation>
+        </message>
+    </context>
+</TS>
---
+++ Projeto 5/Codigo_Projeto_5/translations/translation_en_US.ts
@@ -0,0 +1,46 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>UHUM</source>
+            <comment>Text</comment>
+            <translation type="obsolete">UHUM</translation>
+        </message>
+        <message>
+            <source>Ola</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Ola</translation>
+        </message>
+        <message>
+            <source>Ola humano</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Ola humano</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Olaaaa huumaanoo</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Olaaaa huumaanoo</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Que tristeza</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Que tristeza</translation>
+        </message>
+    </context>
+</TS>
